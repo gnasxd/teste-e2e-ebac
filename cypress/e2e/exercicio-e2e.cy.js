a435984aef065021d37e22edd3b73381af2a5704
Guilherme Augusto <guibiston@gmail.com>
2025-04-10 22:09:17 -0300

    teste e2e

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,46 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil-2.json')
+
+import produtosPage from "../support/page_objects/produtos.page"
+
+
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
 
-  beforeEach(() => {
-      cy.visit('/')
-  });
+    beforeEach(() => {
+        cy.visit('/minha-conta')
+        produtosPage.visitarUrl
+    });
 
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
+
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+        cy.get('#username').type('gnas.teste@testecy.com.br')
+        cy.get('#password').type('teste@1234')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gnas.testegnasxdgnasxd (não é gnas.testegnasxdgnasxd? Sair)')
+
+        let produto = 'Abominable Hoodie'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+
+        produtosPage.visitarProduto('Arcadio Gym Short')
+        cy.get('.product_title').should('contain', 'Arcadio Gym Short')
+
+        produtosPage.visitarProduto('Atlas Fitness Tank')
+        cy.get('.product_title').should('contain', 'Atlas Fitness Tank')
+
+        produtosPage.visitarProduto('Aether Gym Pant')
+        cy.get('.product_title').should('contain', 'Aether Gym Pant')
+
+        let qtd = 1
+        produtosPage.buscarProduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('M', 'Red', qtd)
+
+       
+        
+
+
+    });
 
 
 })
