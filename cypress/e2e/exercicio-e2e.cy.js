b91dc7317d171612587ba37e6f1fbedff1ebadb8
Guilherme Augusto <guibiston@gmail.com>
2025-04-12 17:43:45 -0300

    modificações no e2e

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 const perfil = require('../fixtures/perfil-2.json')
 
-import produtosPage from "../support/page_objects/produtos.page"
+import produtosPage from '../support/page_objects/produtos.page';
 
 
 
@@ -19,27 +19,37 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gnas.testegnasxdgnasxd (não é gnas.testegnasxdgnasxd? Sair)')
 
-        let produto = 'Abominable Hoodie'
+        let produto = 'Atomic Endurance Running Tee (Crew-Neck)'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain', produto)
 
-        produtosPage.visitarProduto('Arcadio Gym Short')
-        cy.get('.product_title').should('contain', 'Arcadio Gym Short')
+        produtosPage.visitarProduto('Augusta Pullover Jacket')
+        cy.get('.product_title').should('contain', 'Augusta Pullover Jacket')
 
         produtosPage.visitarProduto('Atlas Fitness Tank')
         cy.get('.product_title').should('contain', 'Atlas Fitness Tank')
 
-        produtosPage.visitarProduto('Aether Gym Pant')
-        cy.get('.product_title').should('contain', 'Aether Gym Pant')
+        produtosPage.visitarProduto('Bruno Compete Hoodie')
+        cy.get('.product_title').should('contain', 'Bruno Compete Hoodie')
 
-        let qtd = 1
-        produtosPage.buscarProduto('Abominable Hoodie')
-        produtosPage.addProdutoCarrinho('M', 'Red', qtd)
+    
+        produtosPage.buscarProduto('Atlas Fitness Tank')
+        produtosPage.addProdutoCarrinho('M', 'Blue', 1)
+        cy.get('.woocommerce-message').should('contain', ' “Atlas Fitness Tank” foi adicionado no seu carrinho.')
 
+        produtosPage.buscarProduto('Atomic Endurance Running Tee (Crew-Neck)')
+        produtosPage.addProdutoCarrinho('M', 'Blue', 1)
+        cy.get('.woocommerce-message').should('contain', '“Atomic Endurance Running Tee (Crew-Neck)” foi adicionado no seu carrinho.')
+
+        produtosPage.buscarProduto('Augusta Pullover Jacket')
+        produtosPage.addProdutoCarrinho('M', 'Blue', 1)
+        cy.get('.woocommerce-message').should('contain', '“Augusta Pullover Jacket” foi adicionado no seu carrinho.')
+
+        produtosPage.buscarProduto('Bruno Compete Hoodie')
+        produtosPage.addProdutoCarrinho('M', 'Blue', 1)
+        cy.get('.woocommerce-message').should('contain', '“Bruno Compete Hoodie” foi adicionado no seu carrinho.')
        
-        
-
-
+      
     });
 
 
--- cypress/support/page_objects/produtos.page.js
+++ cypress/support/page_objects/produtos.page.js
@@ -15,13 +15,16 @@ class ProdutosPage {
         cy.visit(`produtos/${urlFormatada}`)
     }
 
-    addProdutoCarrinho(tamanho, cor, quantidade) {
-        cy.get('.button-variable-item-' + tamanho).click()
-        cy.get(`.button-variable-item-${cor}`).click()
-        cy.get('.input-text').clear().type(quantidade)
+    addProdutoCarrinho() {
+        cy.get('.button-variable-item-M').click()
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.input-text').clear().type(1)
         cy.get('.single_add_to_cart_button').click()
+      
   
     }
+
+  
 }
 
 export default new ProdutosPage()
