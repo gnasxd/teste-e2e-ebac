f369e81ee410c2517e8a7f5a8335074390aff7cc
Guilherme Augusto <guibiston@gmail.com>
2025-12-02 19:21:41 -0300

    mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -8,3 +8,26 @@ module.exports = defineConfig({
     baseUrl: "http://lojaebac.ebaconline.art.br/"
   },
 });
+
+module.exports = defineConfig({
+  // 1. Defina o reporter aqui
+  reporter: 'cypress-mochawesome-reporter',
+  
+  reporterOptions: {
+    reportDir: 'cypress/reports', // Onde o relatório será salvo
+    charts: true,
+    reportPageTitle: 'Relatório de Testes',
+    embeddedScreenshots: true,
+    inlineAssets: true, // Cria um arquivo HTML único (fácil de compartilhar)
+    saveAllAttempts: false,
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // 2. Adicione o plugin aqui dentro do setupNodeEvents
+      require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
+    },
+    // ... restante da sua configuração (baseUrl, etc)
+  },
+});
